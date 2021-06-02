86c17b01a506ea03aeded7ab63ca3a1326ed6c27
adevaraz <zaraveda.zv@gmail.com>
2021-06-02 20:30:24 +0700

    modify date validator

--- src/routes/logbook/Entri.js
+++ src/routes/logbook/Entri.js
@@ -2,10 +2,10 @@ import * as entriController from '../../controller/logbook/Entri'
 import * as ValidatorSanitizer from '../../middleware/logbook/InputValidatorSanitizer'
 import express from 'express'
 const router = express.Router()
-router.post('/create/:id_logbook', entriController.createEntri)
+router.post('/create/:id_logbook', ValidatorSanitizer.postNewEntri, entriController.createEntri)
 router.get('/:id', entriController.getEntri)
 router.put('/update/:id', ValidatorSanitizer.updateEntriById, entriController.updateEntri)
-router.put('/update-by-date/:tanggal', ValidatorSanitizer.updateEntriById, entriController.updateEntriByDate)
+router.put('/update-by-date/:tanggal', entriController.updateEntriByDate)
 router.delete('/delete/:id_logbook', ValidatorSanitizer.deleteEntriById, entriController.removeEntri)
 router.delete('/delete-by-date/:id_logbook', ValidatorSanitizer.deleteEntriByDate, entriController.removeEntriByDate)
 export default router
