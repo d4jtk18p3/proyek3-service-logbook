31832dbd295f67415d4ea880cc0996206264609a
Ananda Bayu <anandabayu090@gmail.com>
2021-07-02 13:09:21 +0700

    fix comment line
    *un-comment line on Entri.js
    
    Co-Authored-By: khoirunnisapk <48423384+khoirunnisapk@users.noreply.github.com>

--- src/routes/logbook/Entri.js
+++ src/routes/logbook/Entri.js
@@ -6,6 +6,6 @@ router.post('/create/:nim', validatorSanitizer.postNewEntri, entriController.cre
 router.get('/:id', entriController.getEntri)
 router.put('/update/:id_logbook', validatorSanitizer.updateEntriById, entriController.updateEntri)
 router.put('/update-by-date/:tanggal', entriController.updateEntriByDate)
-// router.delete('/delete/:id_logbook', validatorSanitizer.deleteEntriById, entriController.removeEntri)
-// router.delete('/delete-by-date/:id_logbook', validatorSanitizer.deleteEntriByDate, entriController.removeEntriByDate)
+router.delete('/delete/:id_logbook', validatorSanitizer.deleteEntriById, entriController.removeEntri)
+router.delete('/delete-by-date/:id_logbook', validatorSanitizer.deleteEntriByDate, entriController.removeEntriByDate)
 export default router
